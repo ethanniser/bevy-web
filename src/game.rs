5308c93119a9d7b7b291a569c1c0ff2c68146218
Ethan Niser <100045248+ethanniser@users.noreply.github.com>
2023-11-28 17:05:05 +0000

    push

--- src/game.rs
+++ src/game.rs
@@ -1,8 +1,10 @@
 use rand::prelude::*;
 
+pub type Grid = [[u16; 4]; 4];
+
 #[derive(Clone)]
-struct Game {
-    board: [[u16; 4]; 4],
+pub struct Game {
+    board: Grid,
     score: u32,
     rng: ThreadRng,
 }
@@ -13,8 +15,34 @@ impl std::cmp::PartialEq for Game {
     }
 }
 
+impl std::fmt::Debug for Game {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut output = String::new();
+
+        output.push_str(&format!("Score: {}\n", self.score));
+
+        for row in self.board.iter() {
+            for tile in row.iter() {
+                output.push_str(&format!("{:4}", tile));
+            }
+            output.push('\n');
+        }
+        write!(f, "{}", output)
+    }
+}
+
+impl std::convert::From<Grid> for Game {
+    fn from(value: Grid) -> Self {
+        Game {
+            board: value,
+            score: 0,
+            rng: thread_rng(),
+        }
+    }
+}
+
 #[derive(Copy, Clone, Debug)]
-enum Direction {
+pub enum Direction {
     Up,
     Down,
     Left,
@@ -22,13 +50,21 @@ enum Direction {
 }
 
 impl Game {
-    fn new() -> Self {
+    pub fn new() -> Self {
         let game = Game {
             board: [[0; 4]; 4],
             score: 0,
             rng: thread_rng(),
         };
         game.add_random_tile().add_random_tile()
+    }
+
+    pub fn score(&self) -> u32 {
+        self.score
+    }
+
+    pub fn board(&self) -> Grid {
+        self.board
     }
 
     fn add_random_tile(&self) -> Game {
@@ -45,23 +81,157 @@ impl Game {
             return new_board;
         }
         let (x, y) = empty_tiles[new_board.rng.gen_range(0..empty_tiles.len())];
-        new_board.board[y][x] = if new_board.rng.gen_range(0..4) == 0 { 4 } else { 2 };
+        new_board.board[y][x] = if new_board.rng.gen_range(0..4) == 0 {
+            4
+        } else {
+            2
+        };
 
         new_board
     }
 
-    // Returns the new game state if the move was valid, or None if the move was invalid.
-    fn move_tiles(&self, direction: Direction) -> Option<Game> {
-        todo!();
+    pub fn move_tiles(&self, direction: Direction) -> Game {
+        match direction {
+            Direction::Up => self.move_up(),
+            Direction::Down => self.move_down(),
+            Direction::Left => self.move_left(),
+            Direction::Right => self.move_right(),
+        }
     }
 
-    fn is_game_over(&self) -> bool {
-        [Direction::Up, Direction::Down, Direction::Left, Direction::Right]
-            .iter()
-            .all(|dir| self.move_tiles(*dir) == None)
+    fn move_up(&self) -> Game {
+        let mut new_board = self.clone();
+        for x in 0..4 {
+            let mut y = 0;
+            while y < 4 {
+                if new_board.board[y][x] == 0 {
+                    let mut y2 = y + 1;
+                    while y2 < 4 {
+                        if new_board.board[y2][x] != 0 {
+                            new_board.board[y][x] = new_board.board[y2][x];
+                            new_board.board[y2][x] = 0;
+                            break;
+                        }
+                        y2 += 1;
+                    }
+                }
+                y += 1;
+            }
+        }
+        new_board
     }
 
-    fn reset(&mut self) {
+    fn move_down(&self) -> Game {
+        todo!()
+    }
+
+    fn move_left(&self) -> Game {
+        todo!()
+    }
+
+    fn move_right(&self) -> Game {
+        todo!()
+    }
+
+    pub fn is_game_over(&self) -> bool {
+        todo!()
+    }
+
+    pub fn reset(&mut self) {
         *self = Game::new();
     }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn pretty_print() {
+        let game = Game::new();
+        println!("{:?}", game);
+
+        let game = Game::from([
+            [2, 4, 8, 16],
+            [0, 2, 4, 8],
+            [0, 0, 2, 4],
+            [0, 0, 0, 2],
+        ]);
+
+        println!("{:?}", game);
+    }
+
+    #[test]
+    fn row_collapse() {
+        let before = [0, 0, 2, 2];
+        let after = [0, 0, 0, 4];
+        assert_eq!(slide_row_foward(before), after);
+
+        let before = [2, 2, 0, 0];
+        let after = [0, 0, 0, 4];
+        assert_eq!(slide_row_foward(before), after);
+
+        let before = [2, 0, 2, 0];
+        let after = [0, 0, 0, 4];
+        assert_eq!(slide_row_foward(before), after);
+
+        let before = [0, 2, 0, 2];
+        let after = [0, 0, 0, 4];
+        assert_eq!(slide_row_foward(before), after);
+
+        let before = [2, 2, 2, 0];
+        let after = [0, 0, 4, 2];
+        assert_eq!(slide_row_foward(before), after);
+
+        let before = [2, 2, 2, 2];
+        let after = [0, 0, 4, 4];
+        assert_eq!(slide_row_foward(before), after);
+
+        let before = [2, 2, 4, 4];
+        let after = [0, 0, 4, 8];
+        assert_eq!(slide_row_foward(before), after);
+    }
+}
+
+fn slide_row_foward(mut row: [u16; 4]) -> [u16; 4] {
+    // first slide over
+    for i in 0..3 {
+        let cur = row[i];
+        let next = row[i + 1];
+
+        if next == 0 {
+            // next is empty so slide over
+            row[i] = 0;
+            row[i + 1] = cur;
+        }
+    }
+
+    // then collapse same cells
+    for i in 0..3 {
+        let cur = row[i];
+        let next = row[i + 1];
+
+        if cur != 0 {
+            if next == cur {
+                // they are the same so collapse
+                row[i] = 0;
+                row[i + 1] = cur + next;
+            }
+        }
+    }
+
+    // then slide over again
+
+    for i in 0..3 {
+        let cur = row[i];
+        let next = row[i + 1];
+
+        if next == 0 {
+            // next is empty so slide over
+            row[i] = 0;
+            row[i + 1] = cur;
+        }
+    }
+
+    row
 }
--- src/main.rs
+++
@@ -1,3 +0,0 @@
-mod game;
-
-fn main() {}
